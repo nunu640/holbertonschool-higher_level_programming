cd31f0a1e935aee733e72ca1fc1eb516c7188a10
nunu640 <nuranea640@gmail.com>
2025-05-02 09:46:28 +0000

    task4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv
-    argc = len(argv) - 1
-
-    if argc == 0:
+    num_args = len(sys.argv) - 1  # argv[0] proqramın adı olduğuna görə -1
+    if num_args == 0:
         print("0 arguments.")
-    elif argc == 1:
+    elif num_args == 1:
         print("1 argument:")
+        print(f"1: {sys.argv[1]}")
     else:
-        print(f"{argc} arguments:")
-
-    for i in range(1, len(argv)):
-        print(f"{i}: {argv[i]}")
+        print(f"{num_args} arguments:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
 ~                                                                                                                                                                                                                                        
 ~                                               
