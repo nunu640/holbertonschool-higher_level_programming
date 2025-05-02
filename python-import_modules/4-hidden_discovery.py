a3794c6ac881bfdccd8717ffbb040e12c54a76b5
nunu640 <nuranea640@gmail.com>
2025-05-02 12:25:16 +0000

    task4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-import importlib.util
-import sys
-def get_module_names(module_path):
-    spec = importlib.util.spec_from_file_location("hidden_4", module_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    names = [name for name in dir(module) if not name.startswith('__')]
-    return sorted(names)
 if __name__ == "__main__":
-    module_path = '/tmp/hidden_4.pyc'
-    names = get_module_names(module_path)
-    for name in names:
-        print(name)
+    import hidden_4
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
