7b58e21cb4c7edf5f9fb54d057c743d342591b75
nunu640 <nuranea640@gmail.com>
2025-05-02 09:31:37 +0000

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1  
-    if num_args == 0:
+    import sys
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
         print("0 arguments.")
-    elif num_args == 1:
+    elif argc == 1:
         print("1 argument:")
-        print(f"1: {sys.argv[1]}")
     else:
-        print(f"{num_args} arguments:")
-        for i in range(1, num_args + 1):
-            print(f"{i}: {sys.argv[i]}")
+        print(f"{argc} arguments:")
+
+    for i in range(argc):
+        print(f"{i + 1}: {argv[i]}")
+
 ~                                                                                                                                                                                                                                        
 ~                                               
